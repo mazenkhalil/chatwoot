68e7c098ef3b6ac4f9bc559b0c16f87ccefe79ff
Mazen Khalil <mazen_k@outlook.com>
2025-03-26 11:19:49 +0300

    increase lock timeout

--- app/jobs/webhooks/facebook_events_job.rb
+++ app/jobs/webhooks/facebook_events_job.rb
@@ -1,12 +1,12 @@
 class Webhooks::FacebookEventsJob < MutexApplicationJob
   queue_as :default
-  retry_on LockAcquisitionError, wait: 1.second, attempts: 8
+  retry_on LockAcquisitionError, wait: 2.seconds, attempts: 8
 
   def perform(message)
     response = ::Integrations::Facebook::MessageParser.new(message)
 
     key = format(::Redis::Alfred::FACEBOOK_MESSAGE_MUTEX, sender_id: response.sender_id, recipient_id: response.recipient_id)
-    with_lock(key) do
+    with_lock(key, 10.seconds) do
       process_message(response)
     end
   end
